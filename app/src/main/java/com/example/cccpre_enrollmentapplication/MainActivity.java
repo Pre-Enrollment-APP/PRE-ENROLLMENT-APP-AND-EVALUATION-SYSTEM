843cc82cd6473ac9a8c2188e1c3de575115e0536
Joanna Marie <joannamarieochoada@gmail.com>
2022-04-12 02:07:29 +0800

    login done

--- app/src/main/java/com/example/cccpre_enrollmentapplication/MainActivity.java
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.cccpre_enrollmentapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+Handler handler;
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent (MainActivity.this,login.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        }, 3000);
     }
 }
---
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/login.java
@@ -0,0 +1,15 @@
+package com.example.cccpre_enrollmentapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logine_page);
+
+    }
+}
