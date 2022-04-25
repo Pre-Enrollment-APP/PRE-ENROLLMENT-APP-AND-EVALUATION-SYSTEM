910e04013e581b56971fd9fbb83458c23b4d98d2
Joanna Marie Ochoada <joannamarieochoada@gmail.com>
2022-04-25 22:16:37 +0800

    pending pre enrollment form

--- app/src/main/java/com/example/cccpre_enrollmentapplication/login.java
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/login.java
@@ -10,7 +10,6 @@ import android.view.View;
 import java.util.logging.Logger;
 
 public class login extends AppCompatActivity {
-public Button button;
 public Button Login;
 
     @Override
@@ -18,7 +17,6 @@ public Button Login;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logine_page);
         Login=(Button) findViewById(R.id.login);
-        button=(Button) findViewById(R.id.register);
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,13 +25,7 @@ public Button Login;
             }
         });
         //create account
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(login.this,SignUp.class);
-                        startActivity(intent);
-            }
-        });
+
 
 
     }
