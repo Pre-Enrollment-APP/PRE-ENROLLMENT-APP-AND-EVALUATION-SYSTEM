6d46d846edd5c3dc502e690454186a57cc582175
danielb <bokuwanivre@gmail.com>
2022-05-01 14:06:25 +0800

    et

--- app/src/main/java/com/example/cccpre_enrollmentapplication/Home_Page.java
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/Home_Page.java
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.content.Intent;
-import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class Home_Page extends AppCompatActivity {
 public Button pre_enrollment;
@@ -14,15 +14,45 @@ public Button evaluate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homepage);
-        pre_enrollment=(Button) findViewById(R.id.preenroll);
+        setContentView(R.layout.activity_main);
 
-        pre_enrollment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(Home_Page.this,Pre_Enrollment.class);
-                startActivity(intent);
-            }
-        });
+        // By ID we can use each component
+        // which id is assign in xml file
+        // use findViewById() to get the
+        // CalendarView and TextView
+        CalendarView calendar = (CalendarView)
+                findViewById(R.id.calendar);
+        TextView date_view = (TextView)
+                findViewById(R.id.date_view);
+
+        // Add Listener in calendar
+        calendar
+                .setOnDateChangeListener(
+                        new CalendarView
+                                .OnDateChangeListener() {
+                            @Override
+
+                            // In this Listener have one method
+                            // and in this method we will
+                            // get the value of DAYS, MONTH, YEARS
+                            public void onSelectedDayChange(
+                                    @NonNull CalendarView view,
+                                    int year,
+                                    int month,
+                                    int dayOfMonth)
+                            {
+
+                                // Store the value of date with
+                                // format in String type Variable
+                                // Add 1 in month because month
+                                // index is start with 0
+                                String Date
+                                        = dayOfMonth + "-"
+                                        + (month + 1) + "-" + year;
+
+                                // set this date in TextView for Display
+                                date_view.setText(Date);
+                            }
+                        });
     }
 }
