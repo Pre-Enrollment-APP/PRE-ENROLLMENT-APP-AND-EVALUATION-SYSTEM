44b5818fa2f04be69425743790d88bb267a8597d
Joanna Marie Ochoada <joannamarieochoada@gmail.com>
2022-05-01 19:54:17 +0800

    Pre_enrollment to

--- app/src/main/java/com/example/cccpre_enrollmentapplication/Home_Page.java
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/Home_Page.java
@@ -9,13 +9,15 @@ import android.view.View;
 
 public class Home_Page extends AppCompatActivity {
 public Button pre_enrollment;
-public Button evaluate;
+public Button activity_about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
         pre_enrollment=(Button) findViewById(R.id.preenroll);
+        activity_about=(Button) findViewById(R.id.evaluate);
+
 
         pre_enrollment.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +26,15 @@ public Button evaluate;
                 startActivity(intent);
             }
         });
+
+        activity_about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent lol=new Intent(Home_Page.this,About.class);
+                startActivity(lol);
+            }
+        });
+
+
     }
 }
