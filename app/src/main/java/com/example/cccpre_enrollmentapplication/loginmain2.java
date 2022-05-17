93b3619778820cfb791edbf12e569dd387a78f77
danielb <bokuwanivre@gmail.com>
2022-05-17 11:59:10 +0800

    konti plng

---
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/loginmain2.java
@@ -0,0 +1,14 @@
+package com.example.cccpre_enrollmentapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginmain2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginmain2);
+    }
+}
