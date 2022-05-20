8fcda08600f96003ce38b1e52a577341ca27e502
danielb <bokuwanivre@gmail.com>
2022-05-20 22:25:32 +0800

    nex

---
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/infosheet2pageONE.java
@@ -0,0 +1,14 @@
+package com.example.cccpre_enrollmentapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class infosheet2pageONE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.infosheet2page_one);
+    }
+}
