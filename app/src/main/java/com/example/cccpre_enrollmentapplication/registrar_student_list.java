d5e48748d1b1132d32a7676baed6e38f1f3d13a2
danielb <bokuwanivre@gmail.com>
2022-05-21 23:01:08 +0800

    student list added. not completed

---
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/registrar_student_list.java
@@ -0,0 +1,14 @@
+package com.example.cccpre_enrollmentapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrar_student_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrar_student_list);
+    }
+}
