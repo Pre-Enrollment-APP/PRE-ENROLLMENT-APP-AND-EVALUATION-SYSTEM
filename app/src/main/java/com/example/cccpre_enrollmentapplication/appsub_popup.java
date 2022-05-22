104cd460d5e65046ddaa517a7cc1634f67bce50a
Joanna Marie Ochoada <joannamarieochoada@gmail.com>
2022-05-22 22:04:13 +0800

    error solved

--- app/src/main/java/com/example/cccpre_enrollmentapplication/appsub_popup.java
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/appsub_popup.java
@@ -2,14 +2,30 @@ package com.example.cccpre_enrollmentapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
-import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.view.View;
+
 
 public class appsub_popup extends AppCompatActivity {
-
+public ImageButton menu_page;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.appsub_popup);
+
+
+        menu_page=findViewById(R.id.menuicon);
+        menu_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(appsub_popup.this,tabmenu.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
