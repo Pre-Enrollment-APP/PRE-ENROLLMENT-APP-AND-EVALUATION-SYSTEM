201715367cd79e313dc6967ddb7458dccf5a40f8
danielb <bokuwanivre@gmail.com>
2022-05-22 20:24:39 +0800

    appsub_popup pangalan nung file

--- app/src/main/java/com/example/cccpre_enrollmentapplication/appsub_popup.java
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/appsub_popup.java
@@ -3,6 +3,7 @@ package com.example.cccpre_enrollmentapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class appsub_popup extends AppCompatActivity {
 
