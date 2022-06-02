fc7102449ccb3dfb8343c5f52a48c3cae35b88ec
danielb <bokuwanivre@gmail.com>
2022-06-02 19:13:25 +0800

    table test

---
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/test_table2.java
@@ -0,0 +1,14 @@
+package com.example.cccpre_enrollmentapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class test_table2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_table2);
+    }
+}
