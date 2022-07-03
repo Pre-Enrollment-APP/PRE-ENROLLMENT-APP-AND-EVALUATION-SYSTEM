dadf13eaa0d4a3a8f3643720f087a2a755ac68de
Joanna Marie Ochoada <joannamarieochoada@gmail.com>
2022-07-04 00:59:45 +0800

    update

--- app/src/main/java/com/example/cccpre_enrollmentapplication/User.java
+++ app/src/main/java/com/example/cccpre_enrollmentapplication/User.java
@@ -2,12 +2,18 @@ package com.example.cccpre_enrollmentapplication;
 
 public class User {
 
-   public String Name,Course,Email;
+   public String Name,Course,Email,Birthday,Father,Mother,Address,Number;
 
 
-   public User(String Name, String Course, String Email ){
+   public User(String Name, String Course, String Email, String Birthday,String Father, String Mother, String Address, String Number ){
       this.Name=Name;
       this.Course=Course;
       this.Email=Email;
+      this.Birthday=Birthday;
+      this.Father=Father;
+      this.Mother=Mother;
+      this.Address=Address;
+      this.Number=Number;
+
    }
 }
